7d222d88ffe574574c2a9cf216ad9096e786b114
Mejdi Ourir <m.ourir@ivsweb.com>
2020-03-06 17:59:08 +0100

    add footer

---
+++ src/components/footer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { rhythm } from '../utils/typography';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer
+        style={{
+          marginTop: rhythm(2.5),
+          paddingTop: rhythm(1),
+        }}
+      >
+        <a
+          href="https://mobile.twitter.com/mejdiourir"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          twitter
+        </a>{' '}
+        &bull;{' '}
+        <a
+          href="https://github.com/idjem"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github
+        </a>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
